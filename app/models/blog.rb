c6f9fbe3811ed08a488e8564330f650de63626a6
Matthew Todd <matthew.todd@gmail.com>
2008-11-12 10:55:45 +0300

    More layout influenced by introduction of Blog type.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < Content
   validates_presence_of :title, :slug
   validates_format_of :slug, :with => /^[a-z0-9-]+$/
+
+  def navigation_children
+    []
+  end
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,10 +1,11 @@
 class Content < ActiveRecord::Base
   named_scope :positioned, :order => :position
+  named_scope :recent, :order => 'created_at DESC', :limit => 10
   named_scope :top_level, :conditions => { :parent_id => nil }
 
   acts_as_list :scope => :parent
   belongs_to :parent, :class_name => 'Content'
-  has_many :children, :class_name => 'Content', :foreign_key => 'parent_id', :order => :position, :dependent => :destroy
+  has_many :children, :class_name => 'Content', :foreign_key => 'parent_id', :dependent => :destroy
 
   attr_accessor :continue_editing
   has_attached_file :photo, :styles => { :large => '500x500#', :gallery => '368x500#', :small => '120x120#' }, :default_style => :large
@@ -15,6 +16,10 @@ class Content < ActiveRecord::Base
 
   def move_lower=(*args)
     move_lower
+  end
+
+  def navigation_children
+    children
   end
 
   def path
