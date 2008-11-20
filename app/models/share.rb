2df4ae0aaf5c64962d5207aa8aaf9f56a012ed1e
Matthew Todd <matthew.todd@gmail.com>
2008-11-20 16:37:16 +0300

    Send email after_create Share.

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,7 +1,14 @@
 class Share < ActiveRecord::Base
+  after_create :deliver_share_created
   attr_accessor :from, :to, :message
   belongs_to :content
 
   validates_presence_of :from, :to
   validates_email_veracity_of :from, :to, :message => 'is invalid'
+
+  private
+
+  def deliver_share_created
+    Mailer.deliver_share_created(self)
+  end
 end
--- test/unit/share_test.rb
+++ test/unit/share_test.rb
@@ -7,4 +7,15 @@ class ShareTest < ActiveSupport::TestCase
   should_not_allow_values_for :from, 'bob'
   should_not_allow_values_for :to, 'bob'
   should_have_instance_methods :message, :message=
+
+  context 'with an unsaved Share' do
+    setup { @share = Factory.build(:share) }
+
+    context 'save' do
+      setup { @share.save! }
+      before_should 'send email' do
+        Mailer.expects(:deliver_share_created).with(@share)
+      end
+    end
+  end
 end
