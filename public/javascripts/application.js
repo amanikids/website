54812040c46de931f16155b21379d9b7684fbf4b
Matthew Todd <matthew.todd@gmail.com>
2008-11-25 11:10:29 +0300

    Unobtrusive javascript for Share this Page submission.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
-jQuery(document).ready(function($) {
-  $('a[rel*=facebox]').facebox()
+$.ajaxSetup({
+  'beforeSend': function(xhr) { xhr.setRequestHeader('Accept', 'text/javascript') }
+});
+
+$(document).ready(function() {
+  $('a[rel*=facebox]').facebox();
+});
+
+$(document).bind('reveal.facebox', function() {
+  $('#facebox form').submit(function() {
+    $.post($(this).attr('action'), $(this).serialize(), function(data) { $.facebox(data) })
+    return false;
+  })
 })
