fd7c4a2c43570e69b3b784ed06bcdc1d0d8b055e
Matthew Todd <matthew.todd@gmail.com>
2008-11-26 14:30:22 +0300

    Sorting photos + admin "layout."

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,12 @@ class Photo < ActiveRecord::Base
   belongs_to :content
   has_attached_file :photo, :styles => { :large => '500x500#', :gallery => '368x500#', :small => '120x120#' }, :default_style => :large, :whiny_thumbnails => true
   validates_attachment_presence :photo
+
+  def move_higher=(*args)
+    move_higher
+  end
+
+  def move_lower=(*args)
+    move_lower
+  end
 end
