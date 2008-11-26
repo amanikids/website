75f4a5f1d45e51800bf4c0bff44b92c0bca73439
Matthew Todd <matthew.todd@gmail.com>
2008-11-26 12:07:10 +0300

    Slideshow!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,14 @@ module ApplicationHelper
     parents.join(' ')
   end
 
+  # FIXME should work with sizes other than the default (see _gallery.html.haml)
   def slideshow_tag(content)
-    image_tag content.photo.url(:large), :size => '500x500'
+    case content.photos.count
+    when 0
+    when 1
+      image_tag content.photo.url, :size => '500x500'
+    else
+      render :partial => 'photos/slideshow', :locals => { :photos => content.photos }
+    end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,9 @@ $.ajaxSetup({
   'beforeSend': function(xhr) { xhr.setRequestHeader('Accept', 'text/javascript') }
 });
 
+// ============================================================================
+// = Facebox Stuff                                                            =
+// ============================================================================
 $(document).ready(function() {
   $('a[rel*=facebox]').facebox();
 });
@@ -12,4 +15,37 @@ $(document).bind('reveal.facebox', function() {
     $.post($(this).attr('action'), $(this).serialize(), function(data) { $.facebox(data) });
     return false;
   })
-})
+});
+
+// ============================================================================
+// = Slideshow Stuff                                                          =
+// ============================================================================
+jQuery.fn.nextSibling = function() {
+  var children = $(this).parent().children();
+  return children.eq((children.index(this) + 1) % children.length);
+};
+
+jQuery.fn.slideshow = function() {
+  $(this).siblings().css('z-index', '-2');
+  $(this).css('z-index', '-1');
+
+  var currentSlide = $(this);
+  setTimeout(function() {
+    currentSlide.fadeIn(1000, function() { // how long to take for the transition
+      $(this).siblings().hide();
+      $(this).nextSibling().slideshow();
+    });
+  }, 2000); // how long to show the slide
+};
+
+$(document).ready(function() {
+  $('.slideshow').each(function() {
+    $(this).children().eq(0).show();
+  });
+});
+
+$(window).load(function() {
+  $('.slideshow').each(function() {
+    $(this).children().eq(1).slideshow();
+  });
+});
