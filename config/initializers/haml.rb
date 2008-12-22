76cdf30ac89baa7874bd625b4073f06d0c5a25f7
Matthew Todd <matthew.todd@gmail.com>
2008-12-22 11:07:35 +0300

    ActiveMerchant configuration, initializer cleanup.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,5 +1,4 @@
-# Apparently you have to say this when Haml's a vendored gem but not installed
-# outside this project.
+# Apparently you have to say this when Haml's a vendored gem but not installed outside this project.
 Haml.init_rails(binding)
 
 # Tell Haml to wrap html attributes with double instead of single quotes.
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,5 @@
-Paperclip.options[:image_magick_path] = '/opt/local/bin' if Rails.env.production?
+# convert and identify are installed in /opt/local both on my machine (via MacPorts) and in production.
+Paperclip.options[:image_magick_path] = '/opt/local/bin'
+
+# missing attachments have a default image here:
 Paperclip::Attachment.default_options[:default_url] = '/images/:attachment/:style/missing.jpg'
