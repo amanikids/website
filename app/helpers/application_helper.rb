982ccf91625ea1b9e59778344a3bf9f90c4e2833
Matthew Todd <matthew.todd@gmail.com>
2008-12-30 15:57:48 +0300

    Specify a custom amount for PayPal donations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,8 +19,38 @@ module ApplicationHelper
     counter.zero? ? {} : { :style => 'display:none' }
   end
 
+  def monthly_donation_form(donation_page, amount = nil, &block)
+    donation_form(monthly_donation_fields(donation_page.paypal_account, amount, donation_page.currency), &block)
+  end
+
+  def one_time_donation_form(donation_page, amount = nil, &block)
+    donation_form(one_time_donation_fields(donation_page.paypal_account, amount, donation_page.currency), &block)
+  end
+
   def slideshow_tag(content, options = {})
     options.reverse_merge! :size => '500x500', :style => :large
     render :partial => (content.photos.any? ? content.photos : [Photo.new]), :locals => { :style => options[:style], :size => options[:size], :slideshow => true }
+  end
+
+
+  private
+
+  def donation_form(fields = {})
+    concat(form_tag(ActiveMerchant::Billing::Integrations::Paypal.service_url, :method => :get))
+    yield
+    concat(content_tag(:div, fields.map { |name, value| hidden_field_tag(name, value) }.join("\n"), :class => :field))
+    concat("\n</form>\n")
+  end
+
+  def donation_fields(account, currency, command, item_name)
+    { :business => account, :currency_code => currency, :cmd => command, :item_name => item_name, :no_note => 1, :no_shipping => 1, :rm => 1 }
+  end
+
+  def monthly_donation_fields(account, amount, currency)
+    { :a3 => amount, :p3 => 1, :src => 1, :t3 => 'M' }.merge(donation_fields(account, currency, '_xclick-subscriptions', "Monthly Donation to Amani Children's Home")).delete_if { |k, v| v.nil? }
+  end
+
+  def one_time_donation_fields(account, amount, currency)
+    { :amount => amount}.merge(donation_fields(account, currency, '_donations', "One-time Donation to Amani Children's Home")).delete_if { |k, v| v.nil? }
   end
 end
