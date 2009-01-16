1f97070fa3d484338052e9fcbdb15019f912d76e
Matthew Todd <matthew.todd@gmail.com>
2009-01-16 15:40:12 +0300

    Search for a specific matching expression.

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -1,2 +1,25 @@
 class Admin::ContentsController < Admin::ApplicationController
+  protected
+
+  def pages
+    @pages = Content.all.reject { |content| content.class == Section || content.class == Blog }
+  end
+
+  def pages_without_text
+    @pages_without_text ||= pages.select { |content| content.body.to_s.length < 50 }
+  end
+
+  def pages_without_photos
+    @pages_without_photos ||= pages.select { |content| content.photos.count.zero? }
+  end
+
+  def pages_matching_expression
+    @pages_matching_expression ||= pages.select { |content| content.any_text_matches? /#{expression}/i }
+  end
+
+  def expression
+    @expression ||= params[:expression] || 'todo|link'
+  end
+
+  helper_method :pages_without_text, :pages_without_photos, :pages_matching_expression, :expression
 end
