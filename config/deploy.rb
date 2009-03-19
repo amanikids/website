36996d1c710fbc77e9d889856d9f71d4ca624ae4
Matthew Todd <matthew.todd@gmail.com>
2009-03-19 11:38:45 +0300

    Speed up deployment.
    . remote_cache
    . don't always run migrations
    . create symlinks with one command rather than four

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,38 +6,21 @@ set :user, 'deploy'
 set :use_sudo, false
 
 set :scm, :git
-set :local_repository, '.git'
 set :branch, 'master'
-set :git_shallow_clone, 1
+set :deploy_via, :remote_cache
 set :git_enable_submodules, true
 
 server 'amanikids.joyeurs.com', :web, :app, :db, :primary => true, :user => user
 
-
-def create_symlink(path)
-  run "rm -rf #{latest_release}/#{path}; ln -s #{shared_path}/#{path} #{latest_release}/#{path}"
-end
-
 namespace :deploy do
-  desc '[OVERRIDE] Deploy and run pending migrations.'
-  task :default do
-    migrations
-  end
-
   desc 'Restart the Application'
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
-
-  namespace :shared do
-    desc 'Symlink shared content and configuration'
-    task :symlinks do
-      create_symlink 'config/database.yml'
-      create_symlink 'config/secret.txt'
-      create_symlink 'public/documents'
-      create_symlink 'public/photos'
-    end
-  end
 end
 
-after 'deploy:finalize_update', 'deploy:shared:symlinks'
+after 'deploy:finalize_update' do
+  shared_resources = ['config/database.yml', 'config/secret.txt', 'public/documents', 'public/photos']
+  symlink_commands = shared_resources.map { |path| "rm -rf #{latest_release}/#{path}; ln -s #{shared_path}/#{path} #{latest_release}/#{path}" }
+  run symlink_commands.join(';')
+end
