6eefe76d97863001e2bce63177150a73b3849b72
Matthew Todd <matthew.todd@gmail.com>
2009-08-04 11:20:38 +0300

    Separate document and newsletter areas.

--- app/controllers/admin/documents_controller.rb
+++ app/controllers/admin/documents_controller.rb
@@ -1,12 +1,16 @@
 class Admin::DocumentsController < Admin::ApplicationController
+  def index
+    @documents = Document.all
+  end
+
   def create
     @document = Document.create!(params[:document])
-    redirect_to admin_root_path
+    redirect_to admin_documents_path
   end
 
   def destroy
     @document = Document.find(params[:id])
     @document.destroy
-    redirect_to admin_root_path
+    redirect_to admin_documents_path
   end
 end
--- app/controllers/admin/newsletters_controller.rb
+++ app/controllers/admin/newsletters_controller.rb
@@ -2,9 +2,13 @@ class Admin::NewslettersController < Admin::ApplicationController
   before_filter :load_new_newsletter, :only => %w[new create]
   before_filter :load_newsletter,     :only => %w[edit update destroy]
 
+  def index
+    @newsletters = Newsletter.by_published_on
+  end
+
   def create
     if @newsletter.save
-      redirect_to @newsletter.continue_editing ? edit_admin_newsletter_path(@newsletter) : admin_root_path
+      redirect_to @newsletter.continue_editing ? edit_admin_newsletter_path(@newsletter) : admin_newsletters_path
     else
       render :action => 'new'
     end
@@ -12,7 +16,7 @@ class Admin::NewslettersController < Admin::ApplicationController
 
   def update
     if @newsletter.update_attributes(params[:newsletter])
-      redirect_to @newsletter.continue_editing ? edit_admin_newsletter_path(@newsletter) : admin_root_path
+      redirect_to @newsletter.continue_editing ? edit_admin_newsletter_path(@newsletter) : admin_newsletters_path
     else
       render :action => 'edit'
     end
@@ -20,7 +24,7 @@ class Admin::NewslettersController < Admin::ApplicationController
 
   def destroy
     @newsletter.destroy
-    redirect_to admin_root_path
+    redirect_to admin_newsletters_path
   end
 
   private
