242f58343616fac9a58c50233759c1685c3dfc13
Matthew Todd <matthew.todd@gmail.com>
2009-10-29 15:42:37 +0300

    Move sass stylesheets to app/views/stylesheets.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -3,3 +3,6 @@ Haml.init_rails(binding)
 
 # Tell Haml to wrap html attributes with double instead of single quotes.
 Haml::Template.options[:attr_wrapper] = '"'
+
+# Tell Sass where our stylesheets are.
+Sass::Plugin.options[:template_location] = Rails.root.join('app', 'views', 'stylesheets').to_s
