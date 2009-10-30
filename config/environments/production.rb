f3a6ff0aa8428ed35507d2dd26ee0446ae7eedbe
Matthew Todd <matthew.todd@gmail.com>
2009-10-30 13:21:45 +0300

    Whitespace.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ config.cache_classes = true
 
 # Full error reports are disabled and caching is turned off
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = false # we can't use (page|asset) caching on heroku! 
+config.action_controller.perform_caching             = false # we can't use (page|asset) caching on heroku!
 config.action_view.cache_template_loading            = true
 
 # See everything in the log (default is :info)
