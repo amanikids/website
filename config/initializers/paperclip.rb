4d3ced7da2228f727da143f033b9f99cff99891c
Matthew Todd <matthew.todd@gmail.com>
2009-10-30 12:45:07 +0300

    Fix paperclip deprecation warning.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 # convert and identify are installed in /opt/local both on my machine (via MacPorts) and in production.
-Paperclip.options[:image_magick_path] = '/opt/local/bin'
+Paperclip.options[:command_path] = '/opt/local/bin'
 
 # missing attachments have a default image here:
 Paperclip::Attachment.default_options[:default_url] = '/images/:attachment/:style/missing.jpg'
