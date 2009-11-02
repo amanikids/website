c8162ad0685d830e56dcfbe5b52425b6331eee36
Matthew Todd <matthew.todd@gmail.com>
2009-11-02 15:29:01 +0300

    Separate config/tapsuey.txt.

--- config/initializers/tapsuey.rb
+++ config/initializers/tapsuey.rb
@@ -2,4 +2,4 @@
 # Tapsuey.port     = '3000'
 
 Tapsuey.host     = 'amanikids.org'
-Tapsuey.password = Rails.root.join('config', 'secret.txt').read.strip
+Tapsuey.password = Rails.root.join('config', 'tapsuey.txt').read.strip
