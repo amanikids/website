df89eb3fbd367d73186f2ca787f79f3b90568ef0
Matthew Todd <matthew.todd@gmail.com>
2009-11-03 13:03:39 +0300

    Updating tapsuey.

--- config/initializers/path.rb
+++
@@ -1,6 +0,0 @@
-path = ENV['PATH'].split(File::PATH_SEPARATOR)
-
-path.unshift('/opt/local/bin')
-path.uniq!
-
-ENV['PATH'] = path.join(File::PATH_SEPARATOR)
--- config/initializers/tapsuey.rb
+++
@@ -1,5 +0,0 @@
-# Tapsuey.host     = 'localhost'
-# Tapsuey.port     = '3000'
-
-Tapsuey.host     = 'amanikids.org'
-Tapsuey.password = Rails.root.join('config', 'tapsuey.txt').read.strip
