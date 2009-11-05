79eb9bd88796fc8d0b958b3fc01b805f8588f4a3
Matthew Todd <matthew.todd@gmail.com>
2009-11-05 15:48:18 +0300

    Use paperclip default for missing photos.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,2 @@
 # convert and identify are installed in /opt/local both on my machine (via MacPorts) and in production.
 Paperclip.options[:command_path] = '/opt/local/bin'
-
-# missing attachments have a default image here:
-Paperclip::Attachment.default_options[:default_url] = '/images/:attachment/:style/missing.jpg'
