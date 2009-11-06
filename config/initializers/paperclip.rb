4c04bbdecc03ac1c3465fcf456ad819a3875219e
Matthew Todd <matthew.todd@gmail.com>
2009-11-06 15:13:49 +0300

    Don't enable paperclip yet.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,11 +3,11 @@
 Paperclip.options[:command_path] = '/opt/local/bin'
 
 # S3 configuration!
-if ENV['S3_BUCKET']
-  Paperclip::Attachment.default_options[:storage] = :s3
-  Paperclip::Attachment.default_options[:bucket]  = ENV['S3_BUCKET']
-  Paperclip::Attachment.default_options[:s3_credentials] = {
-    :access_key_id     => ENV['S3_KEY'],
-    :secret_access_key => ENV['S3_SECRET']
-  }
-end
+# if ENV['S3_BUCKET']
+#   Paperclip::Attachment.default_options[:storage] = :s3
+#   Paperclip::Attachment.default_options[:bucket]  = ENV['S3_BUCKET']
+#   Paperclip::Attachment.default_options[:s3_credentials] = {
+#     :access_key_id     => ENV['S3_KEY'],
+#     :secret_access_key => ENV['S3_SECRET']
+#   }
+# end
