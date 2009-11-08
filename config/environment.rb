45214153fea9bb9a991f3aaf6cff3565838b23c6
Matthew Todd <matthew.todd@gmail.com>
2009-11-08 08:48:36 +0300

    Assume recent enough rack is available in deployment.

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   config.gem 'fastercsv'
   config.gem 'haml'
   config.gem 'paperclip'
-  config.gem 'rack'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
