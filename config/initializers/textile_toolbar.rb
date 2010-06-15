c4e311939301f1820f9b97760285974faf334825
Matthew Todd <matthew.todd@gmail.com>
2010-06-15 12:43:53 +0300

    Pull textile_toolbar into lib to fix it.

---
+++ config/initializers/textile_toolbar.rb
@@ -0,0 +1,2 @@
+ActionView::Base.send :include, TextileToolbar
+ActionView::Helpers::FormBuilder.send :include, TextileToolbar::FormBuilder
--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -29,6 +29,3 @@ module TextileToolbar
     end
   end
 end
-
-ActionView::Base.send :include, TextileToolbar
-ActionView::Helpers::FormBuilder.send :include, TextileToolbar::FormBuilder
