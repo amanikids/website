15af322b338aed916fe8846075f344ae20d178f7
Matthew Todd <matthew.todd@gmail.com>
2010-06-15 12:57:36 +0300

    textile_toolbar needed some html_safe love.
    At least it did under Rails 2.3.8.

--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -9,18 +9,18 @@ module TextileToolbar
   def textile_toolbar(id, disable)
     html = javascript_include_tag("textile_toolbar")
     html << link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*', '*')")
-    html << "&nbsp;"
+    html << "&nbsp;".html_safe
     html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_', '_')")
-    html << "&nbsp;"
+    html << "&nbsp;".html_safe
     html << link_to_function(image_tag("textile_toolbar/underline.png", :size => "23x22", :alt => "Make selection underlined"), "surround_selection('#{id}', '+', '+')")
-    html << "&nbsp;"
+    html << "&nbsp;".html_safe
     html << link_to_function(image_tag("textile_toolbar/hyperlink.png", :size => "23x22", :alt => "Make hyperlink"), "insert_hyperlink('#{id}')")
-    html << "&nbsp;"
+    html << "&nbsp;".html_safe
     unless disable == :image
       html << link_to_function(image_tag("textile_toolbar/image.png", :size => "23x22", :alt => "Insert image"), "insert_image('#{id}')")
-      html << "&nbsp;&nbsp;"
+      html << "&nbsp;&nbsp;".html_safe
     end
-    html << "<small>" << link_to("Textile", "http://hobix.com/textile/", :target => "_blank") << "&nbsp;enabled</small>"
+    html << "<small>".html_safe << link_to("Textile", "http://hobix.com/textile/", :target => "_blank") << "&nbsp;enabled</small>".html_safe
   end
 
   module FormBuilder
