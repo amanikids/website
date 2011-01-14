14a782318afb2d8e9ce3cb16fe762bf08a34a1f8
Matthew Todd <matthew.todd@gmail.com>
2011-01-14 09:49:57 +0300

    Performance: don't sort so often!

--- app/models/address_list.rb
+++ app/models/address_list.rb
@@ -5,6 +5,8 @@ class AddressList < ActiveRecord::Base
 
   serialize :addresses, Array
 
+  before_save :uniquify_addresses
+
   def addresses
     read_attribute(:addresses) || write_attribute(:addresses, [])
   end
@@ -12,19 +14,22 @@ class AddressList < ActiveRecord::Base
   def csv_files=(csv_files)
     csv_files.each do |file|
       CSV.parse(file).each do |row|
-        add_unique_addresses(row.grep(EMAIL_ADDRESS_PATTERN))
+        add_addresses(row.grep(EMAIL_ADDRESS_PATTERN))
       end
     end
   end
 
   def subscriptions=(subscriptions)
-    add_unique_addresses(subscriptions.map(&:email))
+    add_addresses(subscriptions.map(&:email))
   end
 
   private
 
-  def add_unique_addresses(values)
+  def add_addresses(values)
     self.addresses.concat(values.flatten.map { |address| address.strip.downcase })
+  end
+
+  def uniquify_addresses
     self.addresses.sort!
     self.addresses.uniq!
   end
