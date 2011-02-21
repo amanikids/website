71240256232348a38e286d4dfdca6a4ccaf99822
Matthew Todd <matthew@matthewtodd.org>
2011-02-21 08:58:09 +0300

    Reorganize environment file.

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,13 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.frameworks -= [ :active_resource ]
-  config.middleware.use 'Rack::CanonicalHost', ENV['CANONICAL_HOST']
-  config.middleware.use 'Rack::Static', :urls => ['/stylesheets'], :root => Rails.root.join('tmp')
+
+  config.middleware.use 'Rack::CanonicalHost',
+    ENV['CANONICAL_HOST']
+
+  config.middleware.use 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => Rails.root.join('tmp')
+
   config.time_zone = 'UTC'
 end
